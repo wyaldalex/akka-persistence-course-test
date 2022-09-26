c1fd7324a35bfb7b6f5049df60b0407e6d248286
AlexAR <moltron_nano@hotmail.com>
2022-09-26 11:09:03 -0600

    feat: serialization fixing class version change it seems

--- src/main/scala/part3_stores_serialization/CustomerSerialization.scala
+++ src/main/scala/part3_stores_serialization/CustomerSerialization.scala
@@ -9,9 +9,9 @@ import com.typesafe.config.ConfigFactory
 //select * from akka.messages where persistence_id = 'user-registration' ALLOW FILTERING;
 
 //command
-case class RegisterUser(email: String, name: String)
+case class RegisterUser(email: String, name: String, userType: String)
 //event
-case class UserRegistered(id: Int, email: String, name: String)
+case class UserRegistered(id: Int, email: String, name: String, userType: String)
 
 
 // serializer
@@ -21,9 +21,9 @@ class UserRegistrationSerializer extends Serializer {
   override def identifier: Int = 53278
 
   override def toBinary(o: AnyRef): Array[Byte] = o match {
-    case event @ UserRegistered(id, email, name) =>
+    case event @ UserRegistered(id, email, name,userType) =>
       println(s"Serializing $event")
-      s"[$id$SEPARATOR$email$SEPARATOR$name]".getBytes()
+      s"[$id$SEPARATOR$email$SEPARATOR$name$SEPARATOR$userType]".getBytes()
     case _ =>
       throw new IllegalArgumentException("only user registration events supported in this serializer")
   }
@@ -34,8 +34,12 @@ class UserRegistrationSerializer extends Serializer {
     val id = values(0).toInt
     val email = values(1)
     val name = values(2)
+    //need to account for the new field
+    val userType = if (values.length == 4) values(3)
+    else  "Common"
 
-    val result = UserRegistered(id, email, name)
+
+    val result = UserRegistered(id, email, name,userType)
     println(s"Deserialized $string to $result")
 
     result
@@ -49,15 +53,15 @@ class UserRegistrationActor extends PersistentActor with ActorLogging {
   var currentId = 0
 
   override def receiveCommand: Receive = {
-    case RegisterUser(email, name) =>
-      persist(UserRegistered(currentId, email, name)) { e =>
+    case RegisterUser(email, name,userType) =>
+      persist(UserRegistered(currentId, email, name,userType)) { e =>
         currentId += 1
         log.info(s"Persisted: $e")
       }
   }
 
   override def receiveRecover: Receive = {
-    case event @ UserRegistered(id, _, _) =>
+    case event @ UserRegistered(id, _, _,userType) =>
       log.info(s"Recovered: $event")
       currentId = id
   }
@@ -76,8 +80,9 @@ object CustomerSerialization  extends App {
   val system = ActorSystem("CustomSerialization", ConfigFactory.load().getConfig("customSerializerDemo"))
   val userRegistrationActor = system.actorOf(Props[UserRegistrationActor], "userRegistration")
 
-    for (i <- 11 to 30) {
-      userRegistrationActor ! RegisterUser(s"user_$i@rtjvm.com", s"User $i")
-    }
+    /*
+    for (i <- 31 to 40) {
+      userRegistrationActor ! RegisterUser(s"user_$i@rtjvm.com", s"User $i", "Super")
+    } */
 
 }
